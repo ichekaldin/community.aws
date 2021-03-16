c46a6d1a42d09fd63bebd54175d5dba7b1e4dc23
ichekaldin <39010411+ichekaldin@users.noreply.github.com>
2021-03-16 13:51:24 +0000

    Remove default values for command_python_version and glue_version
    This is to avoid inadvertently breaking existing playbooks. Defaults
    will be set by AWS.

--- plugins/modules/aws_glue_job.py
+++ plugins/modules/aws_glue_job.py
@@ -57,7 +57,6 @@ options:
     description:
       - AWS Glue version. This determines the available version of Apache Scala and Python as described here
         U(https://docs.aws.amazon.com/glue/latest/dg/add-job.html).
-    default: 0.9
     type: str
   max_concurrent_runs:
     description:
@@ -469,7 +468,7 @@ def main():
             connections=dict(type='list', elements='str'),
             default_arguments=dict(type='dict'),
             description=dict(type='str'),
-            glue_version=dict(type='str', default='0.9'),
+            glue_version=dict(type='str'),
             max_concurrent_runs=dict(type='int'),
             max_retries=dict(type='int'),
             name=dict(required=True, type='str'),
